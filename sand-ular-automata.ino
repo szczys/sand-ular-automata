ca974f098ecd3d47feea7932ffe75228583e2263
Mike Szczys <szczys@hotmail.com>
2019-05-08 20:30:44 -0500

    Formatted for Arduino IDE and started groundwork for CA manipulation

--- sand-ular-automata.ino
+++ sand-ular-automata.ino
@@ -1,6 +1,14 @@
 /*
   Hourglass OLED project
+
+  Dependencies:
+    ESP32 Arduino support https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-mac-and-linux-instructions/
+
+    Clone this forked repo into ~/Arduino/libraries/
+      git@github.com:szczys/esp8266-oled-ssd1306.git
  */
+
+
 
 //I2C version
 //https://github.com/ThingPulse/esp8266-oled-ssd1306
@@ -11,25 +19,37 @@
 // ESP-WROOM-32 has LED on pin 2:
 int led = 2;
 
+uint8_t buff[32*4];
+
 struct Sand
 {
   char x;
   char y;
 };
 
+
+
 // I2C version
 //SSD1306 display(0x3c, 18, 19);
 SH1106Wire display(0x3c, 18, 19);
 struct Sand s1 = {12,12};
+
+void clearBuff(void) { for (uint16_t i=0; i<(32*4); i++) { buff[i] = 0b10101010; } }
+
+void showBuf(uint16_t xoffset, uint16_t yoffset) {
+  display.drawFastImage(xoffset, yoffset, 32, 32, buff);
+}
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
 
+  clearBuff();
   
   display.init();
-  display.drawString(0, 0, "Hello World");
+  display.drawString(0, 0, "Hello Sandular");
+  showBuf(20,20);
   display.display();
 
   
