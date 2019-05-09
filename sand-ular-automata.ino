37a4f5a2206f5f2b15f735230ac3ab102c1350b5
Mike Szczys <szczys@hotmail.com>
2019-05-08 23:06:39 -0500

    First proof of concept or sand cellular automata

--- sand-ular-automata.ino
+++ sand-ular-automata.ino
@@ -16,10 +16,51 @@
 //#include "SSD1306.h"
 #include "SH1106Wire.h"
 
+
+#define GRAINSWIDE  32 //GRAINSWIDE must be divisible by 8
+#define GRAINSDEEP  32
+
 // ESP-WROOM-32 has LED on pin 2:
 int led = 2;
 
-uint8_t buff[32*4];
+uint8_t buff[(GRAINSWIDE/8)*GRAINSDEEP];
+uint8_t lastbuff[(GRAINSWIDE/8)*GRAINSDEEP];
+/*
+uint8_t buff[32*4] = {
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000000,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+  0b11111111, 0b00000000, 0b00000000, 0b00000011,
+};
+*/
 
 struct Sand
 {
@@ -34,10 +75,66 @@ struct Sand
 SH1106Wire display(0x3c, 18, 19);
 struct Sand s1 = {12,12};
 
-void clearBuff(void) { for (uint16_t i=0; i<(32*4); i++) { buff[i] = 0b10101010; } }
+void clearBuff(void) {
+  for (uint16_t i=0; i<((GRAINSWIDE/8)*GRAINSDEEP); i++) {
+    buff[i] = 0;
+    lastbuff[i] = 0;
+  }
+}
 
 void showBuf(uint16_t xoffset, uint16_t yoffset) {
-  display.drawFastImage(xoffset, yoffset, 32, 32, buff);
+  display.clear(); //drawFastImage doesn't draw black pixels so clear first
+  display.drawFastImage(xoffset, yoffset, GRAINSWIDE, GRAINSDEEP, buff);
+}
+
+uint8_t getSand(uint16_t x, uint16_t y) {
+  //uint16_t byteIdx = (x/8)+(y*(GRAINSWIDE/8));
+  //uint16_t byteLoc = x%8;
+
+  //Imagedraw places left to right a top to bottom, and top to bottom as left to right
+  uint16_t byteIdx = x*(GRAINSWIDE/8);
+  uint16_t byteOffset = (y/8);
+  uint16_t byteLoc = y%8;
+  if (lastbuff[byteIdx+byteOffset] & (1<<byteLoc)) return 1;
+  else return 0;  
+}
+
+void setSand(uint16_t x, uint16_t y, uint8_t onoff) {
+  //uint16_t byteIdx = (x/8)+(y*(GRAINSWIDE/8));
+  //uint16_t byteLoc = x%8;
+
+  //Imagedraw places left to right a top to bottom, and top to bottom as left to right
+  uint16_t byteIdx = x*(GRAINSWIDE/8);
+  uint16_t byteOffset = (y/8);
+  uint16_t byteLoc = y%8;
+  if (onoff > 0) { buff[byteIdx+byteOffset] |= (1<<byteLoc); }
+  else { buff[byteIdx+byteOffset] &= ~(1<<byteLoc); }
+}
+
+/*
+ * Cellular automata scheme:
+ * 
+ * if cell below is empty, drop
+ * if cell below is full but below to the left is empty, fall there, otherwise fall sell below to the right
+ * now that individual cells have fallen, check row issues:
+ *   if row above is entirely full, and this row has empty spaces near the edges, move one grain toward that empty space
+ */
+
+void moveSand(void) {
+  //preserve current state
+  for (uint16_t i=0; i<((GRAINSWIDE/8)*GRAINSDEEP); i++) { lastbuff[i] = buff[i]; }
+  
+  /* if cell below is empty, drop */
+  for (uint16_t row=0; row<GRAINSDEEP; row++) {
+    for (uint16_t col=0; col<GRAINSWIDE; col++) {
+      //Check if we should be dropping this grain
+      if (getSand(col,row) && (row < (GRAINSDEEP-1))) {
+        if (getSand(col,row+1) == 0) { setSand(col,row,0); setSand(col,row+1,1); continue; }
+        if (col > 0) { if (getSand(col-1,row+1) == 0) { setSand(col,row,0); setSand(col-1,row+1,1); continue; } }
+        if (col < (GRAINSWIDE-1)) { if (getSand(col+1,row+1) == 0) { setSand(col,row,0); setSand(col+1,row+1,1); } }
+      }
+    }
+  }
 }
 
 // the setup routine runs once when you press reset:
@@ -46,6 +143,14 @@ void setup() {
   pinMode(led, OUTPUT);
 
   clearBuff();
+
+  setSand(0,0,1);
+  setSand(0,31,1);
+  setSand(31,0,1);
+  setSand(16,6,1);
+  setSand(16,8,1);
+  setSand(16,10,1);
+
   
   display.init();
   display.drawString(0, 0, "Hello Sandular");
@@ -61,16 +166,31 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   static int nexttime = millis();
+  static int nextframe = millis() + 100;
   if (millis() > nexttime) {
-    display.clearPixel(s1.x, s1.y);
-    s1.y += 1;
-    display.setPixel(s1.x, s1.y);
+    setSand(16,0,1);    
+    showBuf(20,20);
+    
+    //display.clearPixel(s1.x, s1.y);
+    //s1.y += 1;
+    //display.setPixel(s1.x, s1.y);
     display.display();
     nexttime = millis()+500;
+    if (digitalRead(led)) digitalWrite(led,LOW);
+    else digitalWrite(led,HIGH);
   }
-  
+
+  if (millis() > nextframe) {
+    moveSand();
+    showBuf(20,20);
+    display.display();
+    nextframe = millis()+100;
+  }
+
+  /*
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(100);               // wait for a second
+  */
 }
